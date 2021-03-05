655e6e8582d7a2027c5205bf37dcef1c5e8e94f4
liweisherry <yjrsherry@gmail.com>
2021-03-05 15:50:55 -0500

    Format the script

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,14 +1,15 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
         email = 'test@gmail.com'
         password = 'Test123'
         user = get_user_model().objects.create_user(
-            email = email,
-            password = password
+            email=email,
+            password=password
         )
 
         self.assertEqual(user.email, email)
@@ -25,11 +26,11 @@ class ModelTests(TestCase):
         """Test creating user with no email raisers error"""
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user(None, 'Test123')
-    
+
     def test_create_new_superuser(self):
         user = get_user_model().objects.create_superuser(
             'test@gmail.com',
-            'test123'
+            'Test123'
         )
 
         self.assertTrue(user.is_superuser)
